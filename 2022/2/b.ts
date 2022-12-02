e42e00bd1ccab93b56be10f2be66f235767294db
Max Schmidt <max.schmidt@outlook.de>
2022-12-02 11:02:40 +0100

    add solution
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

---
+++ 2022/2/b.ts
@@ -0,0 +1,22 @@
+const input = Deno.readTextFileSync("./input.txt");
+
+const possibilities = new Map([
+  ["A,X", 3],
+  ["A,Y", 4],
+  ["A,Z", 8],
+  ["B,X", 1],
+  ["B,Y", 5],
+  ["B,Z", 9],
+  ["C,X", 2],
+  ["C,Y", 6],
+  ["C,Z", 7],
+]);
+
+console.log(
+  input
+    .split("\n")
+    .reduce(
+      (prev, cur) => prev + possibilities.get(cur.split(" ").toString())!,
+      0
+    )
+);
