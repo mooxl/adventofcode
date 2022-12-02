40a4b5df5beb378c6ff7adb6545eb583d40b18a1
Max Schmidt <max.schmidt@outlook.de>
2022-12-02 16:41:08 +0100

    finish part one of day three on aoc 2015
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

---
+++ 2015/3/a.ts
@@ -0,0 +1,37 @@
+const input = Deno.readTextFileSync("./input.txt");
+
+const houses: number[][] = [[]];
+
+let x = 1000;
+let y = 1000;
+
+input.split("").forEach((move, i) => {
+  if (i === 0) houses[0][0] = 1;
+  switch (move) {
+    case "^": {
+      y++;
+      break;
+    }
+    case ">": {
+      x++;
+      break;
+    }
+    case "v": {
+      y--;
+      break;
+    }
+    case "<": {
+      x--;
+      break;
+    }
+  }
+  if (houses[x] === undefined) houses[x] = [];
+  if (houses[x][y] === undefined) houses[x][y] = 0;
+  houses[x][y]++;
+});
+const lucky = houses.reduce((prev, cur) => {
+  const thisArr: number[] = cur.filter((thi) => typeof thi === "number");
+  return prev + thisArr.length;
+}, 0);
+
+console.log(lucky);
